45cc30b06bb528fb7d0920f4efa608b774e454fa
WesselWessels <wesselwessels1@Wessels-MacBook-Pro-2.local>
2015-11-03 14:51:54 +0200

    Trying

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ Compass.init(function (method) {
   alert('Compass heading by ' + method);
   });
 
+  Compass.watch(function (heading) {
+  $('.btn').text(heading);
+  $('.btn').css('transform', 'rotate(' + (-heading) + 'deg)');
+});
+
 });
